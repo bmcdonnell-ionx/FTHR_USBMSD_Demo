5c7769b1780760551709d17e6bc510c1c6e6300a
Greg Steiert <41132966+gsteiert@users.noreply.github.com>
2016-11-16 18:25:25 +0000

    USB Mass Storage Device demo with SD card

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
 #include "mbed.h"
+#include "max32630fthr.h"
+#include "USBMSD_SD.h"
 
-#define MAX14690_I2C_ADDR    0x50
-
-DigitalOut led1(LED1);
-I2C i2cm2(P5_7, P6_0);
-
+DigitalOut rLED(LED1);
+DigitalOut gLED(LED2);
 
 // main() runs in its own thread in the OS
 // (note the calls to Thread::wait below for delays)
 int main()
 {
-    char data[5];
-    data[0] = 0x14; // I2C address for first register (LDO2 CFG)
-    data[1] = 0x00; // Dissable LDO2
-    data[2] = 0x19; // Set voltage to 3.3V
-    data[3] = 0x00; // Dissable LDO3
-    data[4] = 0x19; // Set voltage to 3.3V
-    i2cm2.write(MAX14690_I2C_ADDR, data, 5);
-    data[1] = 0x02; // Enable LDO2
-    data[3] = 0x02; // Enable LDO3
-    i2cm2.write(MAX14690_I2C_ADDR, data, 5);
+    gLED = LED_OFF;
+    rLED = LED_ON;
+    max32630fthrInit();
+    Thread::wait(100);
+
+    vddioh(P0_4, TRUE);
+    vddioh(P0_5, TRUE);
+    vddioh(P0_6, TRUE);
+    vddioh(P0_7, TRUE);
+    gLED = LED_ON;
+    Thread::wait(100);
+
+    USBMSD_SD sd(P0_5, P0_6, P0_4, P0_7);  // mosi, miso, sclk, cs
+    rLED = LED_OFF;
 
     while (true) {
-        led1 = !led1;
+        gLED = !gLED;
         Thread::wait(500);
     }
 }
