0a7a1c224c112deb839df22b5b839ff8f48f316b
Brendan McDonnell <bmcdonnell@amsteddigital.com>
2018-08-28 15:16:52 -0400

    replace LF w/ CRLF in console output

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ FileHandle *mbed::mbed_override_console(int fd)
 // (note the calls to Thread::wait below for delays)
 int main()
 {
-    printf("--- Mbed OS filesystem example ---\n");
+    printf("--- Mbed OS filesystem example ---\r\n");
 
     Thread::wait(100);
 
@@ -39,42 +39,42 @@ int main()
     printf("Mounting the filesystem... ");
     fflush(stdout);
     int err = fs.mount(&bd);
-    printf("%s\n", (err ? "Fail :(" : "OK"));
+    printf("%s\r\n", (err ? "Fail :(" : "OK"));
     if (err) {
         // Reformat if we can't mount the filesystem
         // this should only happen on the first boot
         printf("No filesystem found, formatting... ");
         fflush(stdout);
         err = fs.reformat(&bd);
-        printf("%s\n", (err ? "Fail :(" : "OK"));
+        printf("%s\r\n", (err ? "Fail :(" : "OK"));
     }
 
     // Open the numbers file
     printf("Opening \"/fs/numbers.txt\"... ");
     fflush(stdout);
     FILE *f = fopen("/fs/numbers.txt", "r+");
-    printf("%s\n", (!f ? "Fail :(" : "OK"));
+    printf("%s\r\n", (!f ? "Fail :(" : "OK"));
     if (!f) {
         // Create the numbers file if it doesn't exist
         printf("No file found, creating a new file... ");
         fflush(stdout);
         f = fopen("/fs/numbers.txt", "w+");
-        printf("%s\n", (!f ? "Fail :(" : "OK"));
+        printf("%s\r\n", (!f ? "Fail :(" : "OK"));
 
         for (int i = 0; i < 10; i++) {
             printf("\rWriting numbers (%d/%d)... ", i, 10);
             fflush(stdout);
-            err = fprintf(f, "    %d\n", i);
+            err = fprintf(f, "    %d\r\n", i);
             if (err < 0) {
-                printf("Fail :(\n");
+                printf("Fail :(\r\n");
             }
         }
-        printf("\rWriting numbers (%d/%d)... OK\n", 10, 10);
+        printf("\rWriting numbers (%d/%d)... OK\r\n", 10, 10);
 
         printf("Seeking file... ");
         fflush(stdout);
         err = fseek(f, 0, SEEK_SET);
-        printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+        printf("%s\r\n", (err < 0 ? "Fail :(" : "OK"));
     }
 
     // Go through and increment the numbers
@@ -94,43 +94,43 @@ int main()
         fseek(f, pos, SEEK_SET);
     
         // Store number
-        fprintf(f, "    %d\n", number);
+        fprintf(f, "    %d\r\n", number);
     }
-    printf("\rIncrementing numbers (%d/%d)... OK\n", 10, 10);
+    printf("\rIncrementing numbers (%d/%d)... OK\r\n", 10, 10);
 
     // Close the file which also flushes any cached writes
     printf("Closing \"/fs/numbers.txt\"... ");
     fflush(stdout);
     err = fclose(f);
-    printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+    printf("%s\r\n", (err < 0 ? "Fail :(" : "OK"));
 
     // Display the root directory
     printf("Opening the root directory... ");
     fflush(stdout);
     DIR *d = opendir("/fs/");
-    printf("%s\n", (!d ? "Fail :(" : "OK"));
+    printf("%s\r\n", (!d ? "Fail :(" : "OK"));
 
-    printf("root directory:\n");
+    printf("root directory:\r\n");
     while (true) {
         struct dirent *e = readdir(d);
         if (!e) {
             break;
         }
-        printf("    %s\n", e->d_name);
+        printf("    %s\r\n", e->d_name);
     }
 
     printf("Closing the root directory... ");
     fflush(stdout);
     err = closedir(d);
-    printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+    printf("%s\r\n", (err < 0 ? "Fail :(" : "OK"));
 
     // Display the numbers file
     printf("Opening \"/fs/numbers.txt\"... ");
     fflush(stdout);
     f = fopen("/fs/numbers.txt", "r");
-    printf("%s\n", (!f ? "Fail :(" : "OK"));
+    printf("%s\r\n", (!f ? "Fail :(" : "OK"));
 
-    printf("numbers:\n");
+    printf("numbers:\r\n");
     while (!feof(f)) {
         int c = fgetc(f);
         printf("%c", c);
@@ -139,7 +139,7 @@ int main()
     printf("\rClosing \"/fs/numbers.txt\"... ");
     fflush(stdout);
     err = fclose(f);
-    printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+    printf("%s\r\n", (err < 0 ? "Fail :(" : "OK"));
 
 
 
@@ -148,13 +148,13 @@ int main()
 //    printf("Unmounting... ");
 //    fflush(stdout);
 //    err = fs.unmount();
-//    printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+//    printf("%s\r\n", (err < 0 ? "Fail :(" : "OK"));
     
 
     printf("Starting MSD... ");
     msd.disk_initialize();
     err = msd.connect();
-    printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+    printf("%s\r\n", (err < 0 ? "Fail :(" : "OK"));
 
     DigitalOut led1(LED1);
 
